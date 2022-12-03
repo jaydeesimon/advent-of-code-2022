407e6346a024ebd00e7f9f52e7f30c83a181a9a3
Jeffrey Simon <jdsimon@gmail.com>
2022-12-03 14:51:43 -0500

    evaluate .cookie lazily

--- src/aoc_2022/util.clj
+++ src/aoc_2022/util.clj
@@ -2,14 +2,12 @@
   (:require [clj-http.client :as http])
   (:import (java.io File)))
 
-(def cookie (slurp ".cookie"))
-
 (defn fetch-input [day]
   (let [input-filename (str "inputs/" day ".txt")]
     (if (.exists (File. input-filename))
       (slurp input-filename)
       (let [url   (format "https://adventofcode.com/2022/day/%d/input" day)
-            input (:body (http/get url {:headers {"Cookie" cookie}}))]
+            input (:body (http/get url {:headers {"Cookie" (slurp ".cookie")}}))]
         (spit input-filename input)
         input))))
 
