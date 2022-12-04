99ba8ad7c63e644f7de140325e986862eeb67d3a
Jeffrey Simon <jdsimon@gmail.com>
2022-12-04 00:04:15 -0500

    day 3 part 1

---
+++ src/aoc_2022/day03.clj
@@ -0,0 +1,95 @@
+(ns aoc-2022.day03
+  (:require [aoc-2022.util :as util]
+            [nextjournal.clerk :as clerk]
+            [clojure.string :as str]
+            [clojure.set :as set]))
+
+;; # Day 3
+
+;; https://adventofcode.com/2022/day/3
+
+(def input
+  (util/fetch-input 3))
+
+;; ```
+;; A given rucksack always has the same number of items in
+;; each of its two compartments, so the first half of the
+;; characters represent items in the first compartment,
+;; while the second half of the characters represent items
+;; in the second compartment.
+;; ```
+
+;; Let's confirm each rucksack has an even number of items by
+;; selecting only the rucksacks with an odd number and making
+;; sure there are zero.
+(->> input
+     str/split-lines
+     (map count)
+     (filter odd?)
+     count
+     zero?)
+
+;; Since we'll need to take a sequence and split it in half,
+;; let's make a function do that.
+
+(defn split-rucksack [rucksack]
+  (split-at (/ (count rucksack) 2) rucksack))
+
+;; Let's try some examples out.
+(split-rucksack [1 2])
+(split-rucksack [1 2 3 4])
+
+;; What happens if we have an odd number? It won't really matter since odd numbers won't exist but am curious.
+(split-rucksack [1 2 3])
+(split-rucksack [1])
+
+;; Now let's split an actual rucksack
+(split-rucksack "vJrwpWtwJgWrhcsFMMfFFhFp")
+
+;; We also need to calculate the priority of an item.
+(def priority
+  (merge
+    (zipmap "abcdefghijklmnopqrstuvwxyz" (range 1 (inc 26)))
+    (zipmap "ABCDEFGHIJKLMNOPQRSTUVWXYZ" (range 27 (inc 52)))))
+
+;; Let's use the examples and see if we have this right.
+(def examples
+  ["vJrwpWtwJgWrhcsFMMfFFhFp"
+   "jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL"
+   "PmmdzqPrVvPwwTWBwg"
+   "wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn"
+   "ttgJtRGJQctTZtZT"
+   " CrZsJsPPZsGzwwsLwLmpwMDw"])
+
+(clerk/table
+  (->> examples
+       (map (fn [rucksack]
+              (let [[left right] (split-rucksack rucksack)]
+                [(apply str left) (apply str right) (set/intersection (set left) (set right))])))
+       (into [["Left" "Right" "Common"]])))
+
+;; In the examples, there is only one item per rucksack that is common. Is this true in the official input?
+;; Let's calculate the frequencies of the number of common items per rucksack.
+(->> input
+     str/split-lines
+     (map split-rucksack)
+     (map (fn [[left right]]
+            (set/intersection (set left) (set right))))
+     (map count)
+     frequencies)
+
+;; *There is only one common item per rucksack.*
+
+;; # Part 1
+;; ```
+;; What is the sum of the priorities of those item types?
+;; ```
+;; In the set of common items, take the first since we know there will always be only one.
+(->> input
+     str/split-lines
+     (map split-rucksack)
+     (map (fn [[left right]]
+            (set/intersection (set left) (set right))))
+     (map first) ;; take the first
+     (map priority)
+     (reduce +))
