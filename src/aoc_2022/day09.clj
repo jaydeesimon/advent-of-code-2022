b605b1d80b2706e05ce4a58e46b938d00715baff
Jeffrey Simon <jdsimon@gmail.com>
2022-12-11 00:22:02 -0500

    Setup for Day 9

---
+++ src/aoc_2022/day09.clj
@@ -0,0 +1,10 @@
+(ns aoc-2022.day09
+  (:require [aoc-2022.util :as util]))
+
+;; # Day 9
+
+;; Source available on [GitHub.](https://github.com/jaydeesimon/advent-of-code-2022/blob/main/src/aoc_2022/day09.clj)
+
+;; https://adventofcode.com/2022/day/9
+
+(def input (util/fetch-input 9))
