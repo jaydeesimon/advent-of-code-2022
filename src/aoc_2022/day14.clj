bd6130a3de854017072d4facad45cafa0f55354a
Jeffrey Simon <jdsimon@gmail.com>
2022-12-16 22:11:01 -0500

    Setup Day 14

---
+++ src/aoc_2022/day14.clj
@@ -0,0 +1,10 @@
+(ns aoc-2022.day14
+  (:require [aoc-2022.util :as util]))
+
+;; # Day 14
+
+;; Source available on [GitHub.](https://github.com/jaydeesimon/advent-of-code-2022/blob/main/src/aoc_2022/day14.clj)
+
+;; https://adventofcode.com/2022/day/14
+
+(def input (util/fetch-input 14))
