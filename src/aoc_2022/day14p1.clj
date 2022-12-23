5d5ff1ba25068e6deb34c03a9401a9984445f9bd
Jeffrey Simon <jdsimon@gmail.com>
2022-12-23 15:38:26 -0500

    Setup Day 15

--- src/aoc_2022/day14p1.clj
+++ src/aoc_2022/day14p1.clj
@@ -5,7 +5,7 @@
 
 ;; # Day 14
 
-;; Source available on [GitHub.](https://github.com/jaydeesimon/advent-of-code-2022/blob/main/src/aoc_2022/day14.clj)
+;; Source available on [GitHub.](https://github.com/jaydeesimon/advent-of-code-2022/blob/main/src/aoc_2022/day14p1.clj)
 
 ;; https://adventofcode.com/2022/day/14
 
---
+++ src/aoc_2022/day15.clj
@@ -0,0 +1,13 @@
+(ns aoc-2022.day15
+  (:require [aoc-2022.util :as util]))
+
+;; # Day 15
+
+;; Source available on [GitHub.](https://github.com/jaydeesimon/advent-of-code-2022/blob/main/src/aoc_2022/day15.clj)
+
+;; https://adventofcode.com/2022/day/15
+
+(def input (util/fetch-input 15))
+
+;; # Part 1
+
