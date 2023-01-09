4752af9013d280cc613585e64231b160203c2ab7
Jeffrey Simon <jdsimon@gmail.com>
2023-01-08 23:52:54 -0500

    Day 16 Setup

---
+++ src/aoc_2022/day16.clj
@@ -0,0 +1,12 @@
+(ns aoc-2022.day16
+  (:require [aoc-2022.util :as util]
+            [clojure.string :as str]
+            [nextjournal.clerk :as clerk]))
+
+;; # Day 16
+
+;; Source available on [GitHub.](https://github.com/jaydeesimon/advent-of-code-2022/blob/main/src/aoc_2022/day16.clj)
+
+;; https://adventofcode.com/2022/day/16
+
+(def input (util/fetch-input 16))
